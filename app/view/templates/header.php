36788b38c0a988cb9733bcc57b8b4c9a86cc44cb
ElfiWG <elfiwijayamangulo@gmail.com>
2026-07-18 12:42:30 +0700

    header

---
+++ app/view/templates/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tiket Parkir Mahasiswa</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="navbar">
+        <div class="container">
+            <h1 class="logo">🅿️ TIKETPARKIRMAHASISWA</h1>
+            <nav>
+                <a href="index.php?url=home/index">Home</a>
+                <a href="index.php?url=parkir/index">Kelola Parkir</a>
+            </nav>
+        </div>
+    </header>
+    <main class="container content">
