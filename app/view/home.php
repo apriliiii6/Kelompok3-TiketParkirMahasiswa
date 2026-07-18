b9f29c59f081bf4daa9d9e5ea6c6cc8f7d957fad
anggietalovelia <anggietalovelia270@gmail.com>
2026-07-18 12:48:35 +0700

    tampilan home

---
+++ app/view/home.php
@@ -0,0 +1,7 @@
+<div class="hero">
+    <h1>Selamat Datang di Tiket Parkir Mahasiswa</h1>
+    <p>Sistem Pengelolaan Tiket Parkir Khusus Mahasiswa Berbasis Web MVC Native.</p>
+    <div class="cta-buttons">
+        <a href="index.php?url=parkir/index" class="btn btn-primary">Masuk ke Dashboard Kelola Parkir</a>
+    </div>
+</div>
