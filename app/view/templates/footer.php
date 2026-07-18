e87e1e3be4d31aeee52fc6c93611c9e11f7b4370
ElfiWG <elfiwijayamangulo@gmail.com>
2026-07-18 12:44:15 +0700

    footerr

---
+++ app/view/templates/footer.php
@@ -0,0 +1,8 @@
+</main>
+    <footer class="footer">
+        <div class="container">
+            <p>&copy; 2026 Kelompok Pemrograman Web I. All Rights Reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
